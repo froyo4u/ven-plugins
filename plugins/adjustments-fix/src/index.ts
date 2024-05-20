7d86141f466cde21cf1c9730611d97486a15ffca
froyo4u <evopoopy@gmail.com>
2024-05-20 13:14:38 -0400

    dude idk wtf I'm doing

--- plugins/adjustments-fix/src/index.ts
+++ plugins/adjustments-fix/src/index.ts
@@ -1,7 +1,6 @@
 import { findByProps } from "@vendetta/metro";
 import { after, before } from "@vendetta/patcher";
 import { storage } from "@vendetta/plugin";
-import fileString from "./lib/fileString";
 
 // Default settings
 storage.nameLength ??= 8;
@@ -13,19 +12,14 @@ export const onUnload = after("uploadLocalFiles", uploadModule, (args) => {
     if (!items) return;
 
     for (const i of items) {
-        // https://github.com/Vendicated/Vencord/blob/7c514e4b1dae25f48b20bc6d5f3025c22e231450/src/plugins/anonymiseFileNames.ts#L70-L71
-        const extIdx = i.filename.lastIndexOf(".");
-        const ext = extIdx !== -1 ? i.filename.slice(extIdx) : "";
-
-        const name = fileString;
-
         // why are there two. why???
         // and yes, i checked, setting both is required...
-        if (ext == "png" || ext == "jpg" || ext == "jpeg") {
+        if (i.filename == "Adjustments.plist") {
             i.filename = "image.png";
-            if (i.item) i.item.filename = "image.png";
+        }
+        if (i.item) {
+            if (i.item.filename == "Adjustments.plist") 
+                i.item.filename = "image.png";
         }
     }
 });
-
-export { default as settings } from "./Settings";
